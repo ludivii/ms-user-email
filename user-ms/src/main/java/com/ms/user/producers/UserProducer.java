c0a49c01221e5bc2be7fecb5aa74ab93fa70af49
Luan <luandsvt@gmail.com>
2024-12-05 09:34:50 -0300

    Creation of EmailDTO, config and producer, added to save the publishMessageEmail method

---
+++ user-ms/src/main/java/com/ms/user/producers/UserProducer.java
@@ -0,0 +1,31 @@
+package com.ms.user.producers;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.ms.user.dto.EmailDTO;
+import com.ms.user.models.UserModel;
+
+@Component
+public class UserProducer {
+
+	final RabbitTemplate rabbitTemplate;
+	
+	public UserProducer(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	@Value(value = "${broker.queue.email.name}")
+	private String routingKey;
+	
+	public void publishMessageEmail(UserModel userModel) {
+		var emailDTO = new EmailDTO();
+		emailDTO.setUserId(userModel.getUserId());
+		emailDTO.setEmailTo(userModel.getEmail());
+		emailDTO.setSubject("Cadastro realizado com sucesso!");
+		emailDTO.setText(userModel.getName() + "seja bem vindo(a)! \nAgradecemos o seu cadastro, aproveite agora todos os recursos da nossa plataforma!");
+		
+		rabbitTemplate.convertAndSend("", routingKey, emailDTO);
+	}
+}
--- user-ms/src/main/java/com/ms/user/servicies/UserService.java
+++ user-ms/src/main/java/com/ms/user/servicies/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 
 @Service
@@ -11,12 +12,17 @@ public class UserService {
 	
 	final UserRepository userRepository;
 	
-	public UserService(UserRepository userRepository) {
+	final UserProducer userProducer;
+	
+	public UserService(UserRepository userRepository, UserProducer userProducer) {
 		this.userRepository = userRepository;
+		this.userProducer = userProducer;
 	}
 	
 	@Transactional
 	public UserModel save (UserModel userModel) {
-		return userRepository.save(userModel);
+		userModel = userRepository.save(userModel);
+		userProducer.publishMessageEmail(userModel);
+		return userModel;
 	}
 }
